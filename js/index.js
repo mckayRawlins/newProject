e14ec7e177dda1f78f4dc759d74f3185c07aca42
Vivian <vivian.bonilla@outlook.com>
2025-01-08 08:56:12 -0700

    Update

--- js/index.js
+++ js/index.js
@@ -3,10 +3,8 @@ let uncomfortableness = 0
 
 function uncomfy(number) {
  uncomfortableness = uncomfortableness + number;
- document.getElementById("noclick").innerHTML = stardust.toFixed(1);
+ document.getElementById("uncomfycounter").innerHTML = uncomfortableness; 
 }
-
-document.getElementById("uncomfycounter").innerHTML = uncomfortableness; 
 
 window.setInterval(function(){
     document.getElementById("uncomfycounter").innerHTML = uncomfortableness;    
