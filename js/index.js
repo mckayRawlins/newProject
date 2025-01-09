3c3ace354f22de182bef129ac4f902342e97cafa
Vivian <vivian.bonilla@outlook.com>
2025-01-09 09:31:25 -0700

    Update

--- js/index.js
+++ js/index.js
@@ -24,7 +24,14 @@ let hobbies = bussinesshobbies[hobbygen]
 
 document.getElementById("hobbies").innerHTML = hobbies;
 
-const bussinesspictures = ["https://i.pinimg.com/736x/64/4c/26/644c268d39e55223bc8e4605a19c2679.jpg","https://i.pinimg.com/736x/c6/86/12/c68612646261196f9f51effbddf667f3.jpg", "https://i.pinimg.com/736x/44/33/ff/4433ffcdd07abf0c4a39e412241b4223.jpg","https://i.pinimg.com/736x/cf/b0/2b/cfb02b1b9e7e89410d70263d69920cb8.jpg", "https://i.pinimg.com/736x/d7/4f/f7/d74ff7ddecdac94f57c6d908c8f75411.jpg", "https://i.pinimg.com/736x/a3/f5/ca/a3f5ca54f089c09b2e85b7e40500aecd.jpg", "https://i.pinimg.com/736x/41/08/b6/4108b69195fcdd0ade5203bdeff3d0a7.jpg"]
+const bussinesspictures = ["https://i.pinimg.com/736x/64/4c/26/644c268d39e55223bc8e4605a19c2679.jpg",
+"https://i.pinimg.com/736x/c6/86/12/c68612646261196f9f51effbddf667f3.jpg", 
+"https://i.pinimg.com/736x/44/33/ff/4433ffcdd07abf0c4a39e412241b4223.jpg",
+"https://i.pinimg.com/736x/cf/b0/2b/cfb02b1b9e7e89410d70263d69920cb8.jpg", 
+ "https://i.pinimg.com/736x/d7/4f/f7/d74ff7ddecdac94f57c6d908c8f75411.jpg", 
+"https://i.pinimg.com/736x/a3/f5/ca/a3f5ca54f089c09b2e85b7e40500aecd.jpg", 
+ "https://i.pinimg.com/736x/64/b9/1b/64b91bb83db91c2cfe8a412dbf81e42d.jpg",
+ "https://i.pinimg.com/736x/57/f6/7d/57f67d43f04f039a95fb9a9b42d4318d.jpg"]
 
 let picturegen = Math.floor(Math.random() * bussinesspictures.length)
 
