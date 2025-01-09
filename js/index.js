857ead5b52c82edf7384f3f38403d0ac82f925fb
Vivian <vivian.bonilla@outlook.com>
2025-01-09 08:23:03 -0700

    Update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
+let businessname = 0 
+let businessjob = 0
+let businessdescription = 0
+let businesspicture = 0 
 
-let uncomfortableness = 0
 
-function uncomfy(number) {
- uncomfortableness = uncomfortableness + number;
- document.getElementById("uncomfycounter").innerHTML = uncomfortableness; 
+function business() {
+const bussinessnames = ["Fitzgerald ManBat", "Hamlin Chuck", "William Esquire","Juckson", "D'marcus Williums", "Tyroil Smoochie-wallace", "D'squarius Green Jr.", "Quandale Dingle", "Walter White"]
+
+let namegen = Math.floor(Math.random() * bussinessnames.length)
+
+let name = bussinessnames[namegen]
+
+document.getElementById("name").innerHTML = name;
 }
 
-window.setInterval(function(){
-    document.getElementById("uncomfycounter").innerHTML = uncomfortableness;    
-    }, 1000);
+
